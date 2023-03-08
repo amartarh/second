3975477ca9e9ceab255607c13c91e4e013d43dc9
amartarh <amartarh@gmail.com>
2023-03-08 23:49:41 +0530

    third

---
+++ Print.java
@@ -0,0 +1,15 @@
+
+//package java_projects;
+
+class Custom {
+    void print() {
+        System.out.println("HI");
+    }
+}
+
+public class Print {
+    public static void main(String args[]) {
+        Custom c = new Custom();
+        c.print();
+    }
+}
--- practiceReserve.java
+++ practiceReserve.java
@@ -48,3 +48,4 @@ public class practiceReserve {
     }
 }
 // winter
+// summer
